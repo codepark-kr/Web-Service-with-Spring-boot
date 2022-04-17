4e134df28862cecf48200a63bd1f7b7c15ca56a1
codepakr-kr <codepark.kr@gmail.com>
2022-04-17 19:22:26 +0900

    [ADD] helloDto@HelloController import 구문 추가

--- src/main/java/com/codepark/webservice/web/HelloController.java
+++ src/main/java/com/codepark/webservice/web/HelloController.java
@@ -1,5 +1,6 @@
 package com.codepark.webservice.web;
 
+import com.codepark.webservice.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
